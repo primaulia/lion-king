2b4841039912347e0254145f7b23b1ceb207cc24
Prima Aulia Gusta <primaulia@gmail.com>
2019-07-04 14:40:37 +0800

    push for livecode

---
+++ interface.rb
@@ -0,0 +1,25 @@
+# TODO
+# 0.)
+# Create a test for each the tasks below
+# Test should be written on animal_spec.rb
+
+# 1.)
+# Create `animal.rb`
+# Define an Animal class with an initialize(name) constructor, exposing its name
+
+# 2.)
+# Define three Lion, Meerkat and Warthog classes
+# and the relevant files
+# inherit Animal for these 3 new classes
+
+# 3.)
+# implement a `#talk` instance method which returns a sentence like "Simba roars" for each animal. (Hint: Meerkats bark and Warthog grunt)
+
+# 4.)
+# implement a `::phyla` class method which will return an array of the four phyla of the animal kingdom.
+
+# 5.)
+# Add an #eat(food) instance method in Animal which
+# returns a sentence like "Timon eats a scorpion".
+# Then override this method *ONLY* for the Lion class,
+# and return a sentence like "Simba eats a gazelle. Law of the Jungle!"
